112bb83e5daf95cce967a3f57583710d4b2d471d
Patrick <ctrlaltpat@gmail.com>
2019-02-01 19:14:33 +0000

    basics with useState

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const Todo = ({ todo, index }) => {
+  return <div className="todo">{todo.text}</div>;
+};
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React Hooks.",
+      isCompleted: false
+    },
+    {
+      text: "Drink more water.",
+      isCompleted: false
+    },
+    {
+      text: "Stop playing Brawl Stars.",
+      isCompleted: false
+    }
+  ]);
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
